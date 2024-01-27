c0e33ca91f55f00e5d03dae9e45dd152921af772
hokage-216 <berrylevente@gmail.com>
2024-01-27 01:34:02 -0500

    updating inquirer prompts and markdown generator function

--- Develop/index.js
+++ Develop/index.js
@@ -1,28 +1,61 @@
 // TODO: Include packages needed for this application
-import { input } from '@inquirer/prompts';
-
-const title = await input({ message: 'Enter your name' });
-
-// TODO: Create an array of questions for user input
-questions.prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    
-]);
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown';
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How will you use this app?'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'rawlist',
+            name: 'license',
+            message: 'What is the title of your project?',
+            choices: ['MIT', 'ISC', 'GPL 2.0', 'GPL 3.0', 'No License']
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Who contributed to this project?'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What command is used to run test?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What email address can people contact you with?'
+        },{
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+        },
+        
+    ]).then((data) => {
+        generateMarkdown(data);
+
+    }).catch((err) => {
+        // code block
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,13 +10,69 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'MIT':
+      
+      break;
+    case 'ISC':
+      
+      break;
+    case 'GPL 2.0':
+    
+    break;
+    case 'GPL 3.0:':
+      
+      break;
+    case 'No License':
+      
+      break;
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+${data.license === "No License" ? null : '- [License](#license)'}
+
+## Installation
+
+To install the npm package run this command(s):
+
+ - ${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Tests
+
+To run a test use this command(s):
+
+${data.tests}
+
+## Contributing
+
+${data.contributing}
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+
+${data.license === 'No License' ? null : renderLicenseSection(data.license)}
 `;
+
 }
 
 module.exports = generateMarkdown;
